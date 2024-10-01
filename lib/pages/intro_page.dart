86cf685877b541d64920082aac1d397fe19ea5f6
arda-fikret-azakli <ardafikretazakli@gmail.com>
2024-10-01 23:49:29 +0200

    navigation changed

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -56,7 +56,7 @@ class IntroPage extends StatelessWidget {
             MyButton(text: "Get Started",
             onTap: () {
               // go to menu page
-              Navigator.pushNamed(context, routeName)
+              Navigator.pushNamed(context, '/intropage');
             },
             )
             
